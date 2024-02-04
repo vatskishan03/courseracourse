21e4f9e1a62290d82bad4b1638874927d730f61c
Kishan <98430344+vatskishan03@users.noreply.github.com>
2024-02-05 02:42:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cors = require('cors');  
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
