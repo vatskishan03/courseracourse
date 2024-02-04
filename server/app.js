14689c4379143f526c8d2dd002b71e9d08157ba8
Kishan <98430344+vatskishan03@users.noreply.github.com>
2024-02-04 23:01:20 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');  
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.get('/public/style.css', (req, res) => {
-  res.setHeader('Content-Type', 'text/css');
-  res.sendFile(path.join(__dirname, '..', 'public', 'style.css'));
+    res.setHeader('Content-Type', 'text/css');
+    res.sendFile(path.join(__dirname, '..', 'public', 'style.css'));
 });
 
 app.post("/courses", async (req, res) => {
-  const { company, difficulty, rating } = req.body;
+    const { company, difficulty, rating } = req.body;
 
-  try {
-    const response = await fetch(`/api/handler?company=${company}&difficulty=${difficulty}&rating=${rating}`);
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error(`Error fetching Python response: ${error}`);
-    res.status(500).send('Error in Python function');
-  }
+    try {
+        const response = await fetch(`/api/script?company=${company}&difficulty=${difficulty}&rating=${rating}`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(`Error fetching Python response: ${error}`);
+        res.status(500).send('Error in Python function');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
